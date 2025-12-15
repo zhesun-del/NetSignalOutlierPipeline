76530f854d090a991443ee1f5f25f093e4e8e536
Zhe Sun <injure21@gmail.com>
2025-12-15 10:54:37 -0800

    Delete src/modeling/global_autoencoder/inference_autoencoder.py

--- src/modeling/global_autoencoder/inference_autoencoder.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-
-if __name__ == "__main__":
-    mlflow.set_tracking_uri("http://njbbvmaspd11:5001")
-    mlflow.set_experiment("Autoencoder_Anomaly_Detection")
-    # Load the registered model
-    model = mlflow.pyfunc.load_model("models:/Autoencoder_Anomaly_Detection/1")
-    print(model.metadata)
-    # Now you can use model.predict() with appropriate input
